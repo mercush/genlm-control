288f28fca83d241b8a3fb03562936cfe72e81e10
Sam Ritchie <sam@mentat.org>
2025-01-30 11:56:46 -0700

    formatting

--- genlm_control/tracer.py
+++ genlm_control/tracer.py
@@ -9,6 +9,7 @@ from genlm_grammar.util import format_table
 
 import numpy as np
 
+
 class generation_tree:
     def __init__(self, lm, remaining_mass=0.0, **opts):
         tracer = TraceSWOR()
@@ -59,9 +60,9 @@ class Tracer:
             cur.context = context
 
         if context != cur.context:
-            print(colors.light.red % 'ERROR: trace divergence detected:')
-            print(colors.light.red % 'trace context:', self.cur.context)
-            print(colors.light.red % 'calling context:', context)
+            print(colors.light.red % "ERROR: trace divergence detected:")
+            print(colors.light.red % "trace context:", self.cur.context)
+            print(colors.light.red % "calling context:", context)
             raise ValueError((p, cur))
 
         a = cur.sample()
@@ -78,14 +79,14 @@ class Tracer:
 
 class Node:
     __slots__ = (
-        'idx',
-        'mass',
-        'parent',
-        'token',
-        'child_masses',
-        'active_children',
-        'context',
-        '_mass',
+        "idx",
+        "mass",
+        "parent",
+        "token",
+        "child_masses",
+        "active_children",
+        "context",
+        "_mass",
     )
 
     def __init__(
@@ -138,23 +139,23 @@ class Node:
 
     def graphviz(
         self,
-        fmt_edge=lambda x, a, y: f'{html.escape(str(a))}/{y._mass/x._mass:.2g}',
+        fmt_edge=lambda x, a, y: f"{html.escape(str(a))}/{y._mass/x._mass:.2g}",
         # fmt_node=lambda x: ' ',
         fmt_node=lambda x: (
-            f'{x.mass}/{x._mass:.2g}' if x.mass > 0 else f'{x._mass:.2g}'
+            f"{x.mass}/{x._mass:.2g}" if x.mass > 0 else f"{x._mass:.2g}"
         ),
     ):
         "Create a graphviz instance for this subtree"
         g = Digraph(
-            graph_attr=dict(rankdir='LR'),
+            graph_attr=dict(rankdir="LR"),
             node_attr=dict(
-                fontname='Monospace',
-                fontsize='10',
-                height='.05',
-                width='.05',
-                margin='0.055,0.042',
+                fontname="Monospace",
+                fontsize="10",
+                height=".05",
+                width=".05",
+                margin="0.055,0.042",
             ),
-            edge_attr=dict(arrowsize='0.3', fontname='Monospace', fontsize='9'),
+            edge_attr=dict(arrowsize="0.3", fontname="Monospace", fontsize="9"),
         )
         f = Integerizer()
         xs = set()
@@ -166,13 +167,13 @@ class Node:
                 continue
             for a, y in x.active_children.items():
                 a = y.token if y.token is not None else a
-                g.edge(str(f(x)), str(f(y)), label=f'{fmt_edge(x,a,y)}')
+                g.edge(str(f(x)), str(f(y)), label=f"{fmt_edge(x,a,y)}")
                 q.append(y)
         for x in xs:
             if x.child_masses is not None:
-                g.node(str(f(x)), label=str(fmt_node(x)), shape='box')
+                g.node(str(f(x)), label=str(fmt_node(x)), shape="box")
             else:
-                g.node(str(f(x)), label=str(fmt_node(x)), shape='box', fillcolor='gray')
+                g.node(str(f(x)), label=str(fmt_node(x)), shape="box", fillcolor="gray")
         return g
 
     def downstream_nodes(self):
@@ -207,12 +208,14 @@ class TraceSWOR(Tracer):
             import sys
             from io import BytesIO
 
-            c = converter.SixelConverter(BytesIO(self.root.graphviz()._repr_image_png()))
+            c = converter.SixelConverter(
+                BytesIO(self.root.graphviz()._repr_image_png())
+            )
             c.write(sys.stdout)
         except ImportError:
             import warnings
 
-            warnings.warn('Install imgcat or sixel to enable rendering.')
+            warnings.warn("Install imgcat or sixel to enable rendering.")
             print(self)
 
     def __repr__(self):
