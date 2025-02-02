fa5e60c4bfae2b615d5d59672b1c46c09fc6fadb
benlebrun <benlebrun1@gmail.com>
2025-02-02 16:24:49 -0500

    eos

---
+++ genlm_control/constant.py
@@ -0,0 +1,16 @@
+class EndOfSequence:
+    """Special sentinel token for end-of-sequence."""
+
+    def __repr__(self):
+        return "<EOS>"
+
+    def __radd__(self, other):
+        if isinstance(other, str):
+            return other + "<EOS>"
+        elif isinstance(other, bytes):
+            return other + b"\x00"  # Using null byte as it's rarely used in text
+        elif isinstance(other, (list, tuple)):
+            return type(other)(list(other) + [self])
+
+
+EOS = EndOfSequence()
