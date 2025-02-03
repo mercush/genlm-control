2c441dc67f14c24e8a17ca5bf356f58f582d2838
benlebrun <benlebrun1@gmail.com>
2025-02-03 17:52:17 -0500

    wfsa

--- tests/potential/test_wfsa.py
+++ tests/potential/test_wfsa.py
@@ -1,7 +1,6 @@
 import pytest
 import numpy as np
 from genlm_grammar import WFSA as BaseWFSA, Float
-from genlm_control.potential.base import EOS
 from genlm_control.potential.built_in import WFSA, BoolFSA
 
 
@@ -10,24 +9,44 @@ def float_wfsa():
     """Creates a simple WFSA in float semiring"""
     m = BaseWFSA(Float)
     m.add_I(0, 1.0)
-    m.add_arc(0, b"a"[0], 1, 1.0)
-    m.add_arc(1, b"b"[0], 2, 0.5)
-    m.add_arc(1, b"c"[0], 2, 0.5)
-    m.add_arc(1, b"d"[0], 3, 0.5)  # dead end
+    m.add_arc(0, b"a"[0], 1, 2)
+    m.add_arc(1, b"b"[0], 2, 1)
+    m.add_arc(1, b"c"[0], 2, 1)
+    m.add_arc(1, b"d"[0], 3, 1)  # dead end
     m.add_F(2, 1.0)
     return m
 
 
 @pytest.mark.asyncio
-async def test_wfsa_complete(float_wfsa):
-    pot_F = WFSA(float_wfsa)
-    pot_regex = WFSA.from_regex("a(b|c)")
+async def test_wfsa(float_wfsa):
+    pot = WFSA(float_wfsa)
 
-    await _test_wfsa_complete(pot_F)
-    await _test_wfsa_complete(pot_regex)
+    log_weight = await pot.complete(b"ab")
+    assert np.isclose(log_weight, np.log(2))
+
+    log_weight = await pot.complete(b"ac")
+    assert np.isclose(log_weight, np.log(2))
+
+    log_weight = await pot.complete(b"a")
+    assert log_weight == float("-inf")
+
+    log_weight = await pot.prefix(b"a")
+    assert np.isclose(log_weight, np.log(4))
+
+    log_weight = await pot.prefix(b"c")
+    assert log_weight == float("-inf")
+
+    log_weight = await pot.prefix(b"ab")
+    assert np.isclose(log_weight, np.log(2))
+
+    await pot.assert_logw_next_consistency(b"a")
+    await pot.assert_autoreg_fact(b"a")
 
 
-async def _test_wfsa_complete(pot):
+@pytest.mark.asyncio
+async def test_wfsa_regex():
+    pot = WFSA.from_regex("a(b|c)")
+
     log_weight = await pot.complete(b"ab")
     assert np.isclose(log_weight, np.log(0.5))
 
@@ -37,21 +56,17 @@ async def _test_wfsa_complete(pot):
     log_weight = await pot.complete(b"a")
     assert log_weight == float("-inf")
 
+    log_weight = await pot.prefix(b"a")
+    assert np.isclose(log_weight, 0)
 
-async def test_wfsa_prefix(float_wfsa):
-    pot = WFSA(float_wfsa)
-    have = await pot.prefix(b"a")
-    want = np.log(0.5)
-    assert np.isclose(have, want)
+    log_weight = await pot.prefix(b"c")
+    assert log_weight == float("-inf")
 
+    log_weight = await pot.prefix(b"ab")
+    assert np.isclose(log_weight, np.log(0.5))
 
-@pytest.mark.asyncio
-async def test_properties(float_wfsa):
-    pot = WFSA(float_wfsa)
-    await pot.assert_properties(b"ab")
-
-    pot = WFSA.from_regex("a(b|c)")
-    await pot.assert_properties(b"ab")
+    await pot.assert_logw_next_consistency(b"a")
+    await pot.assert_autoreg_fact(b"a")
 
 
 @pytest.mark.asyncio
@@ -76,25 +91,5 @@ async def test_bool_fsa(float_wfsa):
     log_weight = await pot.prefix(b"ab")
     assert log_weight == 0
 
-    await pot.assert_properties(b"a")
-
-
-# This test is redundant with assert_properties, since if both prefix and complete are correct,
-# then passing assert_properties indicates that logp_next is also correct. We leave it in
-# for extra coverage though.
-@pytest.mark.asyncio
-async def test_wfsa_logp_next(float_wfsa):
-    pot = WFSA(float_wfsa)
-
-    inf = float("-inf")
-
-    vocabulary = [b"a"[0], b"b"[0], b"c"[0], EOS]
-    for input_seq, want in [
-        (b"", [0, inf, inf, inf]),
-        (b"a", [inf, np.log(0.5), np.log(0.5), inf]),
-        (b"ab", [inf, inf, inf, 0]),
-        (b"ac", [inf, inf, inf, 0]),
-    ]:
-        have = (await pot.logp_next(input_seq)).materialize()
-        for i, v in enumerate(vocabulary):
-            assert have[v] == want[i], [v, want, have]
+    await pot.assert_logw_next_consistency(b"a")
+    await pot.assert_autoreg_fact(b"a")
