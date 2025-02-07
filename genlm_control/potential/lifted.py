15cbd2d2f958bd1bf8a60c98943c4fdae434a9ba
benlebrun <benlebrun1@gmail.com>
2025-02-07 13:17:38 -0500

    update lift

--- genlm_control/potential/lifted.py
+++ genlm_control/potential/lifted.py
@@ -1,59 +1,55 @@
-from genlm_control.potential.base import Potential
-from itertools import chain
-
-# single state transducer
+from genlm_control.potential import Potential
 
 
 class Lifted(Potential):
     """Represents a potential lifted to operate on a target vocabulary."""
 
-    def __init__(self, potential, target_vocab, f=None, g=None):
+    def __init__(self, potential, target_vocab, f, g, f_seq):
         self.potential = potential
-        # target_vocab^* -> potential.\Sigma^*
-        self.f = f or (lambda x: list(chain(*x)))
-        # potential.\Sigma^* -> target_vocab
-        self.g = g or (lambda x: bytes(x))
+        self.f = f
+        self.g = g
+        self.f_seq = f_seq
 
         valid_tokens = []
-        for token in target_vocab:
-            token = self.f([token])  # XXX need an abstraction for transduction
-            if set(token) <= set(potential.decode):
-                valid_tokens.append(self.g(token))
+        for target_token in target_vocab:
+            base_token = f(target_token)
+            if set(base_token) <= set(potential.decode):
+                valid_tokens.append(g(base_token))
 
         if not valid_tokens:
             raise ValueError("No valid tokens found in target vocabulary")
 
         super().__init__(valid_tokens)
 
-    def _batch_f(self, contexts):
-        return [self.f(context) for context in contexts]
+    def _batch_f_seq(self, contexts):
+        return [self.f_seq(context) for context in contexts]
 
     async def complete(self, context):
-        return await self.potential.complete(context=self.f(context))
+        return await self.potential.complete(context=self.f_seq(context))
 
     async def prefix(self, context):
-        return await self.potential.prefix(context=self.f(context))
+        return await self.potential.prefix(context=self.f_seq(context))
 
     async def logw_next(self, context):
         Ws = await self.potential.batch_logw_next_seq(
-            context=self.f(context), extensions=self.decode_eos
+            context=self.f_seq(context), extensions=self.decode_eos
         )
         return self.make_lazy_weights(Ws)
 
     async def logw_next_seq(self, context, extension):
         return await self.potential.logw_next_seq(
-            context=self.f(context), extension=self.f(extension)
+            context=self.f_seq(context), extension=self.f_seq(extension)
         )
 
     async def batch_complete(self, contexts):
-        return await self.potential.batch_complete(contexts=self._batch_f(contexts))
+        return await self.potential.batch_complete(contexts=self._batch_f_seq(contexts))
 
     async def batch_prefix(self, contexts):
-        return await self.potential.batch_prefix(contexts=self._batch_f(contexts))
+        return await self.potential.batch_prefix(contexts=self._batch_f_seq(contexts))
 
     async def batch_logw_next_seq(self, context, extensions):
         return await self.potential.batch_logw_next_seq(
-            context=self.f(context), extensions=self._batch_f(extensions)
+            context=self.f_seq(context), extensions=self._batch_f_seq(extensions)
         )
 
     def __repr__(self):
